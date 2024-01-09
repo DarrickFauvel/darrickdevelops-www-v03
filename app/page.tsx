49113ac6704b88df44db55c813b216da9d39dec7
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-01-09 08:33:11 -0500

    Add footer section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import AboutSection from "@/components/About"
+import AboutSection from "@/components/AboutSection"
+import Footer from "@/components/FooterSection"
 import HeroSection from "@/components/HeroSection"
+import ProjectsSection from "@/components/ProjectsSection"
 
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
       <HeroSection />
       <AboutSection />
+      <ProjectsSection />
+      <Footer />
     </main>
   )
 }

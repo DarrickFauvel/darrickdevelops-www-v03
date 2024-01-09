333bc46a604055092ba46a0c7c472856b1b2b51d
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-01-09 07:28:33 -0500

    Update skills image

--- components/About.tsx
+++ components/About.tsx
@@ -73,11 +73,11 @@ const AboutSection = () => {
               })}
             </div>
             <Image
-              src="/hero-image.png"
+              src="/undraw_hello_re_3evm.svg"
               alt=""
-              width={325}
-              height={325}
-              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+              width={280}
+              height={280}
+              className="hidden md:block md:relative md:mt-10 md:left-0 md:z-0"
             />
           </div>
         </div>
